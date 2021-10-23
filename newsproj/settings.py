a07c4a35d835a0f1e822a392a988863e1873653d
sarangkkl <72507795+sarangkkl@users.noreply.github.com>
2021-10-23 12:36:14 +0530

    Update settings.py

--- newsproj/settings.py
+++ newsproj/settings.py
@@ -140,9 +140,10 @@ CKEDITOR_CONFIGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
