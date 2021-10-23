07b470b9321201a1a49986ca9d871b9461db80da
sarangkkl <72507795+sarangkkl@users.noreply.github.com>
2021-10-23 12:28:34 +0530

    Update settings.py

--- newsproj/settings.py
+++ newsproj/settings.py
@@ -48,6 +48,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
