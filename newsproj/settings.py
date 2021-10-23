8b8a1545be2e06d6eca5b5adbbb1ee07d82cc728
sarangkkl <72507795+sarangkkl@users.noreply.github.com>
2021-10-23 18:26:31 +0530

    Update settings.py

--- newsproj/settings.py
+++ newsproj/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'newsproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sarang',
+        'USER':'postgres',
+        'PASSWORD': '12345',
+        'HOST':'localhost'
     }
 }
 
